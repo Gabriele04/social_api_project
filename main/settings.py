9a8cbef80d037fe5518501993c39670d120264a0
gabri <gabriele_04@hotmail.it>
2026-07-06 20:10:16 +0200

    Other changes for upload the project on render

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d&gf)+byuum0gz@n^yzu2a$r8^ylj@xcdq^xh-)g1u)$t%$43o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
