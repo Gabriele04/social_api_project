a466c35c57b770d9bca04ad4ddecde8066c5fd51
gabri <gabriele_04@hotmail.it>
2026-07-07 10:36:45 +0200

    Added handler for endpoints not found

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def handler_404(*args, **kwargs):
+    return JsonResponse(
+        {"error": "Endpoint not found", "status_code": 404},
+        status=404
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +14,5 @@ urlpatterns = [
     path('api/posts/', include('posts.urls')),
     path('api/chats/', include('chats.urls')),
 ]
+
+handler404 = 'main.urls.handler_404'
